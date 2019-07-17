aa91a7c989dc0c76721091f51017b3a642b67403
wangruifeng <1371934898@qq.com>
2019-07-17 17:57:50 +0800

    test stash

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ module.exports = function createApp(context) {
     template: `<div>访问的URL是：{{ url }}</div>`
   })
 }
-console.log(13)
+console.log(13 + 'git stash测试')
 console.log('456131256546新分支上的提交')
